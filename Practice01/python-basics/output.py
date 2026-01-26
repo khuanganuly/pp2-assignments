85435bf5cd513f4c0582c8634f8af24c3086860e
Darkhan <darkhan2k8@gmail.com>
2026-01-27 01:30:42 +0500

    Output examples. Printing Text and Numbers

---
+++ Practice01/python-basics/output.py
@@ -0,0 +1,22 @@
+#either double quote or single both works
+print("This will work!")
+print('This will also work!')
+
+
+#use end=" " to print multiple words on the same line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+
+#printing int numbers
+print(3)
+print(358)
+print(50000)
+
+
+print(3 + 3)
+print(2 * 5)
+
+#mix
+print("I am", 35, "years old.")

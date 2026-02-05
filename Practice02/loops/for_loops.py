a8d3691959ae143b593fea1e6751f4101d0f11f6
Darkhan <darkhan2k8@gmail.com>
2026-02-05 17:15:24 +0500

    For While loops

---
+++ Practice02/loops/for_loops.py
@@ -0,0 +1,86 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+
+for x in "banana":
+  print(x)
+
+
+
+#for break
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+
+
+
+#for continue
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+
+
+
+
+for x in range(6):
+  print(x)
+
+
+
+
+for x in range(2, 6):
+  print(x)
+
+
+
+
+for x in range(2, 30, 3):
+  print(x)
+
+
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+
+
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+
+
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+
+#for pass
+
+for x in [0, 1, 2]:
+  pass

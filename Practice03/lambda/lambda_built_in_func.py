8fe9c6377719a96207e44f99804689941c4d4600
Darkhan <darkhan2k8@gmail.com>
2026-02-20 07:51:39 +0500

    Python lambda

--- Practice03/lambda/lambda_built_in_func.py
+++ Practice03/lambda/lambda_built_in_func.py
@@ -1,16 +1,16 @@
 #Lambda with Built-in Functions
 
-#with map
+#with map func
 numbers = [1, 2, 3, 4, 5]
 doubled = list(map(lambda x: x * 2, numbers))
 print(doubled)
 
-#with list
+#with list func
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
 print(odd_numbers)
 
-#with sorted
+#with sorted func
 students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
 sorted_students = sorted(students, key=lambda x: x[1])
 print(sorted_students)

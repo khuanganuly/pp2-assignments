1cc7365c1110b5cc24645733373f9a85f48c66e1
Darkhan <darkhan2k8@gmail.com>
2026-02-20 08:03:02 +0500

    Python inheritence

--- Practice03/inheritance/inheritance_basics.py
+++ Practice03/inheritance/inheritance_basics.py
@@ -48,36 +48,6 @@ class Student(Person):
     Person.__init__(self, fname, lname)
 
 
-#Python also has a super() function that will make the child class inherit all the methods and properties from its parent:
-class Student(Person):
-  def __init__(self, fname, lname):
-    super().__init__(fname, lname)
 
 
 
-
-#add properties
-class Student(Person):
-  def __init__(self, fname, lname):
-    super().__init__(fname, lname)
-    self.graduationyear = 2019
-
-
-
-class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
-
-x = Student("Mike", "Olsen", 2019)
-
-
-
-#add methods
-class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
-
-  def welcome(self):
-    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
